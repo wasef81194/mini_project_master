391d4b01060259230abfc1ffe25f2225d7a32c33
Wasef81194 <alex.wasef@gmail.com>
2024-01-11 17:11:53 +0100

    add commande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Commande;
+use App\Entity\DetailCommande;
+use App\Entity\ProduitPanier;
 use App\Form\CommandeType;
 use App\Repository\CommandeRepository;
+use App\Repository\PanierRepository;
+use App\Repository\ProduitPanierRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,23 +26,43 @@ class CommandeController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_commande_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/new/{idPanier}', name: 'app_commande_new', methods: ['GET'])]
+    public function new($idPanier, PanierRepository $panierRepository,ProduitPanierRepository $produitPanierRepository, EntityManagerInterface $entityManager): Response
     {
+        $panier = $panierRepository->findOneBy(['id'=> $idPanier, 'utilisateur' => $this->getUser()]);
+        $produitsPanier = $produitPanierRepository->findBy(['panier' => $panier, 'supprimer_le' => null]);
+        //Créer la commande
         $commande = new Commande();
-        $form = $this->createForm(CommandeType::class, $commande);
-        $form->handleRequest($request);
+        $commande->setUtilisateur($this->getUser());
+        $commande->setCreerLe(new \DateTime);
+        $entityManager->persist($commande);
+        $entityManager->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($commande);
+        foreach ($produitsPanier as $produitPanier) {
+            //Créer les details de la commande
+            $detailCommande = new DetailCommande();
+            $detailCommande->setCommande($commande);
+            $detailCommande->setProduit($produitPanier->getProduit());
+            $detailCommande->setQuantite($produitPanier->getQuantite());
+            $detailCommande->setPrixUnitaire($produitPanier->getProduit()->getPrix());
+            $detailCommande->setCreerLe(new \DateTime);
+            $entityManager->persist($detailCommande);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_commande_index', [], Response::HTTP_SEE_OTHER);
+            //Supprime les produit du panier
+            $produitPanier->setSupprimerLe(new \DateTime);
+            $entityManager->persist($produitPanier);
+            $entityManager->flush();
         }
 
+        //Reset le panier
+        $panier->setPrixTotal(0);
+        $entityManager->persist($panier);
+        $entityManager->flush();
+      
         return $this->render('commande/new.html.twig', [
             'commande' => $commande,
-            'form' => $form,
+            'message' => 'Votre commande à bien été prise en compte',
         ]);
     }
 
